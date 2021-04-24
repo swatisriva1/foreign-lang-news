b502665f41cea58e9b16014f8e4603c03cf7eab1
swatisriva1 <40495467+swatisriva1@users.noreply.github.com>
2021-04-24 15:27:25 -0400

    Comment out redundant first name function

--- homeDB.php
+++ homeDB.php
@@ -118,20 +118,20 @@ function getArticles($language) {
     return $list_articles;
 }
 
-function getUserFname($username) {
-    global $db;
-    $Fname = "";
-    $query = "SELECT * FROM user_info
-    WHERE username = :username";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username',$username);
-    $statement->execute();
-    $user_result=$statement->fetch();
-    if(!empty($user_result)) {
-        $Fname = $user_result['f_name'];
-    }
-    $statement -> closeCursor();
-    return $Fname;
-}
+// function getUserFname($username) {
+//     global $db;
+//     $Fname = "";
+//     $query = "SELECT * FROM user_info
+//     WHERE username = :username";
+//     $statement = $db->prepare($query);
+//     $statement->bindValue(':username',$username);
+//     $statement->execute();
+//     $user_result=$statement->fetch();
+//     if(!empty($user_result)) {
+//         $Fname = $user_result['f_name'];
+//     }
+//     $statement -> closeCursor();
+//     return $Fname;
+// }
 
 ?>
