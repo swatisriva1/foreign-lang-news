b530f9d998be6db4ede1ec29d62d6652ca4ad68b
Megan Reddy <meganr28@gmail.com>
2021-05-03 19:35:39 -0400

    Increase app title size in navbar

--- settings.php
+++ settings.php
@@ -120,7 +120,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark">
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            <a class="navbar-brand" href="home.php">Hello World</a>
+            <a style="font-size: 30px;" class="navbar-brand" href="home.php">Hello World</a>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <!-- <div class="input-group">
